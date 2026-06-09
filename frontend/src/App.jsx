96dcbdcbc0b7c38370de0b5651e1d47b635067ee
Koby <gravity980530@gmail.com>
2026-06-10 00:39:59 +0900

    fix: Route 내 경로 오류 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { navigate } from 'react-router-dom';
 import Home from './pages/Home';
-import PostDetail from './pages/Postdetail';
+import PostDetail from './pages/PostDetail';
 import Login from './pages/Login';
 import PrivateRoute from './components/PrivateRoute';
 
