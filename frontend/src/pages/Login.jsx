4b24f2c922adf26fda029861948bd937bccb0f55
Koby <gravity980530@gmail.com>
2026-06-10 00:44:53 +0900

    fix: 로그인 페이지 import 모듈 추가

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 function Login() {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
