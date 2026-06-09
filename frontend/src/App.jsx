34624b010793e498e15271b070fbeb8185bdff91
Koby <gravity980530@gmail.com>
2026-06-10 00:36:20 +0900

    feat: 메인 페이지 라우터 및 로그인 기능 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import PostDetail from './pages/PostDetail'
-import Login from './pages/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Postdetail from './pages/Postdetail';
+import Login from './pages/Login';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
-        <Route path="/login" element={<Login />} />
+    <BrowserRouter> {/* 여기서부터 URL 감지용도 */}
+      <Routes>      {/* 아래에 경로 목록들을 정의해둠 */}
+        {/* path = url 주소(경로) / element = 그 경로에서 보여줄 컴포넌트 */}
+        {/* 로그인 페이지는 항상 진입 가능하게 */}
+        <Route path = "/login" element={<Login />} />
+
+        {/* 페이지는 프라이빗하게 운영 예정 */}
+        <Route element = {<PrivateRoute />}>
+          <Route path = "/" element={<Home />} />
+          <Route path = "/post/:id" element={<Postdetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,28 @@
-// Login.jsx
 function Login() {
-    return <div>Login</div>
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    async function handleLogin() {
+        // 백엔드 nest.js에 로그인 요청
+        const res = await fetch ('/api/auth/login', {
+            method : 'POST', 
+            headers : { 'Content-Type' : 'application/json' },
+            body: JSON.stringify({id, password})
+        })
+        const data = await res.json();
+        localStorage.setItem('token', data.token);
+        navigate('/');
+    }
+
+    return (
+        <div>
+            <h1>로그인</h1>
+            <input type = "text" placeholder = "아이디" value = {id} onChange = {e => setId(e.target.value)} />
+            <input type = "password" placeholder = "비밀번호" value = {password} onChange = {e => setPassword(e.target.value)} />
+            <button onClick = {handleLogin}>로그인</button>
+        </div>
+    )
 }
+
 export default Login
